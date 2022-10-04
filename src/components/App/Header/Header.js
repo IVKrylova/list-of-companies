66b59e2f3a7584804c22d480c1e3732b17a67516
IVKrylova <ivk.lis@yandex.ru>
2022-10-04 10:57:07 +0300

    feat: add component Header

---
+++ src/components/App/Header/Header.js
@@ -0,0 +1,11 @@
+import 'Header.css';
+
+const Header = _ => {
+  return (
+    <header className='header'>
+      <h1 className='header__title'>Тестовое задание для Frontend developer</h1>
+    </header>
+  );
+}
+
+export default Header;
