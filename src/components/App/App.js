2aef77c113146ed3791a3052e037ca905e312365
IVKrylova <ivk.lis@yandex.ru>
2022-10-04 11:24:37 +0300

    feat: add Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 const App = _ => {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = _ => {
+  return (
+    <footer className='footer'>
+      <p className="footer__copyright">
+        &copy;{new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
