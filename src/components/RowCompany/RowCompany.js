10e2b2a178dfc71c0ad32292db06e37dbd40418e
IVKrylova <ivk.lis@yandex.ru>
2022-10-12 17:45:17 +0300

    feat: add errors for table of companies

--- src/components/RowCompany/RowCompany.js
+++ src/components/RowCompany/RowCompany.js
@@ -1,23 +1,26 @@
 import Row from '../Row/Row';
+import { useFormAndValidation } from '../../hooks/useFormAndValidation';
 
 const RowCompany = props => {
+  const { values, handleChange, errors, isValid, resetForm, setIsValid } = useFormAndValidation();
+
   const classNameTextares = `row__cell row__cell_textarea ${props.checked ? 'row__cell_checked' : ''}`;
 
-  const handleChange = _ => {
 
-  }
 
   return (
     <Row
       checked={props.checked}
       onClickCheckbox={props.onClickCheckbox}
-      company={props.company}
+      company={props.company || values.company}
     >
       <textarea
         className={classNameTextares}
-        defaultValue={props.name}
+        defaultValue={props.name || values.name}
         name='name'
         onChange={handleChange}
+        required={true}
+        minLength='2'
       >
       </textarea>
       <textarea
@@ -33,8 +36,16 @@ const RowCompany = props => {
         defaultValue={props.address}
         name='address'
         onChange={handleChange}
+        required={true}
+        minLength='10'
       >
       </textarea>
+      <span className={`row__error row__error_type_nameCompany ${!isValid ? 'row__error_active' : ''}`}>
+        {!isValid && errors.name}
+      </span>
+      <span className={`row__error row__error_type_addressCompany ${!isValid ? 'row__error_active' : ''}`}>
+        {!isValid && errors.address}
+      </span>
     </Row>
   );
 }
